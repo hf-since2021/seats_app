f749bd8ffc9fb13f7b7b7f3ec59c54381223bafb
hf-since2021 <hrk.fj.0911mon@gmail.com>
2022-11-25 23:23:06 +0900

    table+inputの練習

--- sheets-web/src/App.js
+++ sheets-web/src/App.js
@@ -1,8 +1,31 @@
 const App = () => {
   return (
-    <>
-      <input></input>
-    </>
+    <table>
+      <thead>
+        <tr>
+          <th>a</th>
+          <th>b</th>
+          <th>c</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><input style={{width: 20}}></input></td>
+          <td>2</td>
+          <td>3</td>
+        </tr>
+        <tr>
+          <td><input style={{width: "20px"}}></input></td>
+          <td>5</td>
+          <td>6</td>
+        </tr>
+        <tr>
+          <td>7</td>
+          <td>8</td>
+          <td>9</td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
 
