05b747ab2c46aed94ec9a0b7e55ae2c67257bd22
hf-since2021 <hrk.fj.0911mon@gmail.com>
2022-11-26 23:37:17 +0900

    tableのアクティブセル移動
    試験的実装その1

--- sheets-web/src/App.js
+++ sheets-web/src/App.js
@@ -1,32 +1,63 @@
 const App = () => {
+  const tableSize = [2,2];
+  const selectText = (e)=>{e.target.select()};
+  const moveFocus = (e) => {
+    const cellId = e.target.id.split("-").map(Number);
+    if((e.ctrlKey && !e.metaKey) || (!e.ctrlKey && e.metaKey)){
+      switch (e.nativeEvent.key) {
+        case "ArrowUp":
+          if(cellId[0]>0){
+            e.preventDefault();
+            cellId[0] --;
+            document.getElementById(cellId.join("-")).focus();
+            document.getElementById(cellId.join("-")).select();
+          };
+          break;
+        case "ArrowLeft":
+          if(cellId[1]>0){
+            e.preventDefault();
+            cellId[1] --;
+            document.getElementById(cellId.join("-")).focus();
+          };
+          break;
+        case "ArrowDown":
+          if(cellId[0]<tableSize[0]){
+            e.preventDefault();
+            cellId[0] ++;
+            document.getElementById(cellId.join("-")).focus();
+          };
+          break;
+        case "ArrowRight":
+          if(cellId[1]<tableSize[1]){
+            e.preventDefault();
+            cellId[1] ++;
+            document.getElementById(cellId.join("-")).focus();
+          };
+          break;
+      };
+    };
+  };
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>a</th>
-          <th>b</th>
-          <th>c</th>
-        </tr>
-      </thead>
+    <table className="sheet-table">
       <tbody>
         <tr>
-          <td><input style={{width: 20}}></input></td>
-          <td>2</td>
-          <td>3</td>
+          <td><input id={"0-0"} onKeyDown={moveFocus}></input></td>
+          <td><input id={"0-1"} onFocus={selectText} onKeyDown={moveFocus}></input></td>
+          <td><input id={"0-2"} onFocus={selectText} onKeyDown={moveFocus}></input></td>
         </tr>
         <tr>
-          <td><input style={{width: "20px"}}></input></td>
-          <td>5</td>
-          <td>6</td>
+          <td><input id={"1-0"} onFocus={selectText} onKeyDown={moveFocus}></input></td>
+          <td><input id={"1-1"} onFocus={selectText} onKeyDown={moveFocus}></input></td>
+          <td><input id={"1-2"} onFocus={selectText} onKeyDown={moveFocus}></input></td>
         </tr>
         <tr>
-          <td>7</td>
-          <td>8</td>
-          <td>9</td>
+          <td><input id={"2-0"} onFocus={selectText} onKeyDown={moveFocus}></input></td>
+          <td><input id={"2-1"} onFocus={selectText} onKeyDown={moveFocus}></input></td>
+          <td><input id={"2-2"} onFocus={selectText} onKeyDown={moveFocus}></input></td>
         </tr>
       </tbody>
     </table>
   );
 }
-
+// if(e.nativeEvent.key=="ArrowDown"){console.log("↓")}
 export default App;
